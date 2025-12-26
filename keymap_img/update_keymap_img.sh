16744c476e7ef54875cb5b906fc9d11ecff5a934
bfk9k <>
2025-12-26 17:13:46 +0100

    rename

--- keymap_img/update_keymap_img.sh
+++ keymap_img/update_keymap_img.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# https://github.com/caksoylar/keymap-drawer#command-line-tool-installation
+# pipx install keymap-drawer
+
 echo 'Parsing ZMK keymap...'
 keymap -c keymap_config.yaml parse -c 12 -z ../config/corne.keymap > keymap.yaml
 
@@ -7,4 +10,4 @@ keymap -c keymap_config.yaml parse -c 12 -z ../config/corne.keymap > keymap.yaml
 # ./keymap_img_adjuster.py keymap.yaml
 
 echo '\n\nDrawing keymap...'
-keymap -c keymap_config.yaml draw --qmk-keyboard corne_rotated --qmk-layout LAYOUT_split_3x6_3 keymap.yaml -s Base Num Nav Bluetooth Gaming > keymap.svg
+keymap -c keymap_config.yaml draw --qmk-keyboard corne_rotated --qmk-layout LAYOUT_split_3x6_3 keymap.yaml -s Base Sym NumNav Fn BT > keymap.svg
